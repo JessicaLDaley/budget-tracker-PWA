3c674740b06ac510e503c26147232031737ab73a
JessicaLDaley <jessicadaleypaints@yahoo.com>
2021-12-01 17:17:01 -0500

    set up db request

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,16 @@
 const FILES_TO_CACHE = [
     "/", 
-    "/index.html", 
-    "/index.js", 
-    "/favicon.ico", 
-    "/styles.css", 
-    "/icons/icon-144x144.png",
-    "/icons/icon-192x192.png", 
-    "/icons/icon-512x512.png"
+    "./public/index.html", 
+    "./public/js/index.js", 
+    "./public/css/styles.css", 
+    "./public/icons/icon-72x72.png",
+    "./public/icons/icon-96x96.png",
+    "./public/icons/icon-128x128.png",
+    "./public/icons/icon-144x144.png",
+    "./public/icons/icon-152x152.png",
+    "./public/icons/icon-192x192.png",
+    "./public/icons/icon-384x384.png",
+    "./public/icons/icon-512x512.png"
   ];
   
   const CACHE_NAME = "static-cache-v2";
@@ -16,10 +20,28 @@ const FILES_TO_CACHE = [
   self.addEventListener("install", function (evt) {
     evt.waitUntil(
       caches.open(CACHE_NAME).then(cache => {
-        console.log("Your files were pre-cached successfully!");
+        console.log("Files pre-cached successfully!");
         return cache.addAll(FILES_TO_CACHE);
       })
     );
   
     self.skipWaiting();
+  });
+
+  // activate
+  self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
   });
