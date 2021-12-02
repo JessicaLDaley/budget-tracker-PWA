63da776ec9120a87e8f41610f9bbab19038426ab
JessicaLDaley <jessicadaleypaints@yahoo.com>
2021-12-01 22:07:42 -0500

    finished checkDatabase function

--- public/js/idb.js
+++ public/js/idb.js
@@ -31,7 +31,38 @@ function saveRecord(record) {
     // add record to your store with add method.
     store.add(record);
   }
+  function checkDatabase() {
+    // open transaction on pending db
+    const transaction = db.transaction(["pending"], "readwrite");
+    // access your pending object store
+    const store = transaction.objectStore("pending");
+    // get all records from store and set to a variable
+    const getAll = store.getAll();
   
+    getAll.onsuccess = function() {
+      if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+          method: "POST",
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+          }
+        })
+        .then(response => response.json())
+        .then(() => {
+          // if successful, open a transaction on pending db
+          const transaction = db.transaction(["pending"], "readwrite");
+  
+          // access pending object store
+          const store = transaction.objectStore("pending");
+  
+          // clear all items in store
+          store.clear();
+        });
+      }
+    };
+  }
 
 // listen for app coming back online
 window.addEventListener("online", checkDatabase);
